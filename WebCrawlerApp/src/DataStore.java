b6cfaed74b03a7ad7472d89260c256c798abaf20
akhil kumar gupta <akhil@akhils-MacBook-Pro.local>
2024-01-07 22:28:37 +0530

    Webcrawler

---
+++ WebCrawlerApp/src/DataStore.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+
+public class DataStore {
+
+    public void store(Data data){
+
+    }
+
+
+}
---
+++ WebCrawlerApp/src/Scheduler.java
@@ -0,0 +1,5 @@
+public class Scheduler {
+    public void delay() {
+
+    }
+    }
